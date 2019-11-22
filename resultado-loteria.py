9ad6efac488cbe48821e4566c91019dbdb41f43f
aco1731 <37146720+aco1731@users.noreply.github.com>
2019-11-21 23:57:41 -0300

    urlAPI

--- resultado-loteria.py
+++ resultado-loteria.py
@@ -15,18 +15,17 @@ async def get_url(url, session):
         return await response.read()
 
 async def busca_resultado(url, session):
+    
+    
+    #Requisição a página principal(BASE_URL) para localizar o endereço da API de resultados.
     html = await get_url(url, session)
-    
-    
-    
     soup = bs4.BeautifulSoup(html, 'html.parser')
-    #Localiza a url BASE e o endpoint da API de resultados.
     urlBase = soup.find("base")['href']
     urlBuscarResultado = soup.find("input", {"id": "urlBuscarResultado", "type":"hidden"})['value']
+    urlAPI = urlBase + urlBuscarResultado
     
-    #Consulta a API.
-    resultado = json.loads(await get_url(urlBase + urlBuscarResultado,session))
-    
+    #Processa o JSON da API.
+    resultado = json.loads(await get_url(urlAPI,session))
     print(url.split('/')[-2].upper()) #Identifica o nome do jogo(ex: Megasena).
     print("Concurso:", resultado['proximoConcurso'])
     print("Resultado:",resultado['resultadoOrdenado'])
